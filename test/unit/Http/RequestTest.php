5cf8ff3bf4b46a2fa4d592f896d5743eeffc07fa
Robert Schönthal <caziel@gmx.net>
2015-05-31 01:32:57 +0200

    fixed test

--- test/unit/Http/RequestTest.php
+++ test/unit/Http/RequestTest.php
@@ -4,9 +4,9 @@ class RequestTest extends PHPUnit_Framework_TestCase
 {
     public function testRequest()
     {
-        $uri = new \phmLabs\Base\Www\Uri('http://smoke.phmlabs.com');
+        $uri = 'http://smoke.phmlabs.com';
         $request = new \whm\Smoke\Http\Request($uri);
 
-        $this->assertEquals($uri, $request->getUrl());
+        $this->assertEquals($uri, $request->getUri());
     }
 }
